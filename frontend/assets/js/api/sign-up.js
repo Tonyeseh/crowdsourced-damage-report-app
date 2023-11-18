4475266b62fba3b8337fd899ca6ef180db521632
esehtony <esehtony@gmail.com>
2023-11-18 23:37:15 +0100

    signup frontend pages

--- frontend/assets/js/api/sign-up.js
+++ frontend/assets/js/api/sign-up.js
@@ -2,12 +2,13 @@ firstName = document.getElementById('firstName')
 lastName = document.getElementById('lastName')
 emailAdd = document.getElementById('emailAdd')
 password = document.getElementById('password')
+confirmPassword = document.getElementById('confirmPassword')
 submitBtn = document.getElementById('submit')
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
 
-    if (firstName.value.length > 3 && lastName.value.length > 3 && emailAdd.value.length > 3 && password.value.length > 5) {
+    if (firstName.value.length > 3 && lastName.value.length > 3 && emailAdd.value.length > 3 && password.value.length > 5 && password.value == confirmPassword.value) {
         form = new FormData()
         form.append('first_name', firstName.value)
         form.append('last_name', lastName.value)
@@ -20,5 +21,10 @@ submitBtn.addEventListener('click', (e) => {
             body: form,
             method: "POST"
         })
+        firstName.value=""
+        lastName.value=""
+        emailAdd.value=""
+        password.value=""
+        confirmPassword.value=""
     }
 })
