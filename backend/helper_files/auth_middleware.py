f0e387fa9b1041c038dd9a0aedff64da64655225
Tonyeseh <esehtony123@gmail.com>
2023-12-19 00:31:47 +0100

    auth_middleware and validation scripts for web_dynamic module

---
+++ backend/helper_files/auth_middleware.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+""" auth_middleware module """
+
+from functools import wraps
+import jwt
+from flask import flash, request, redirect
+from flask import current_app
+from models import storage
+from models.admin_user import AdminUser
+from models.student_user import StudentUser
+from models.worker import Worker
+
+
+def token_required(func):
+    """ wrapper function for token required routes """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        """decorated function"""
+        token = None
+        if request.cookies.get('admin_access_token'):
+            token = request.cookies.get('admin_access_token')
+
+        if not token:
+            return redirect('/admin/login')
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unathorized",
+            }, 401
+        print(token)
+        try:
+            data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            print(data)
+            current_user = storage.get(AdminUser, data['user_id'])
+
+            if current_user is None:
+                return {
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unathorized"
+                }, 401
+            # if not current_user['active']:
+            #     abort(403)
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+        
+        return func(current_user, *args, **kwargs)
+    
+    return decorated
+
+
+def user_token_required(func):
+    """ wrapper function for token required routes """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        """decorated function"""
+        token = None
+        if request.cookies.get('user_access_token'):
+            token = request.cookies.get('user_access_token')
+
+        if not token:
+            flash("Please login in to continue", category='danger')
+            return redirect('login')
+        print(token)
+        try:
+            data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            print(data)
+            current_user = storage.get(StudentUser, data['user_id'])
+
+            if current_user is None:
+                flash("Could not login you in. Try again!", category='danger')
+                return redirect('login')
+            
+            
+        except Exception as e:
+            flash("Something went wrong")
+            return redirect('login')
+        
+        return func(current_user, *args, **kwargs)
+    
+    return decorated
+
+
+def worker_token_required(func):
+    """ wrapper function for token required routes """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        """decorated function"""
+        token = None
+        if request.cookies.get('worker_access_token'):
+            token = request.cookies.get('worker_access_token')
+
+        if not token:
+            return redirect('login')
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unathorized",
+            }, 401
+        print(token)
+        try:
+            data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            print(data)
+            current_user = storage.get(Worker, data['user_id'])
+
+            if current_user is None:
+                return {
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unathorized"
+                }, 401
+            # if not current_user['active']:
+            #     abort(403)
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+        
+        return func(current_user, *args, **kwargs)
+    
+    return decorated
