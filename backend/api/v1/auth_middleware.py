bc68f67cfb1c9069d3dc42c245aa7ab74a47278c
Tonyeseh <esehtony123@gmail.com>
2023-12-19 00:29:48 +0100

    User and worker login token wrapper
    auth_middleware for api

--- backend/api/v1/auth_middleware.py
+++ backend/api/v1/auth_middleware.py
@@ -7,6 +7,8 @@ from flask import request, abort
 from flask import current_app
 from models import storage
 from models.admin_user import AdminUser
+from models.student_user import StudentUser
+from models.worker import Worker
 
 
 def token_required(func):
@@ -46,3 +48,82 @@ def token_required(func):
         return func(current_user, *args, **kwargs)
     
     return decorated
+
+
+def user_token_required(func):
+    """ wrapper function for token required routes """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        """decorated function"""
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers['Authorization'].split(' ')[1]
+
+        if not token:
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unathorized",
+            }, 401
+        try:
+            data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            current_user = storage.get(StudentUser, data['user_id'])
+
+            if current_user is None:
+                return {
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unathorized"
+                }, 401
+            # if not current_user['active']:
+            #     abort(403)
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+        
+        return func(current_user, *args, **kwargs)
+    
+    return decorated
+
+
+def worker_token_required(func):
+    """ wrapper function for token required routes """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        """decorated function"""
+        token = None
+        if "Authorization" in request.headers:
+            token = request.headers['Authorization'].split(' ')[1]
+
+        if not token:
+            return {
+                "message": "Authentication Token is missing!",
+                "data": None,
+                "error": "Unathorized",
+            }, 401
+        try:
+            data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+            current_user = storage.get(Worker, data['user_id'])
+
+            if current_user is None:
+                return {
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unathorized"
+                }, 401
+            # if not current_user['active']:
+            #     abort(403)
+        except Exception as e:
+            return {
+                "message": "Something went wrong",
+                "data": None,
+                "error": str(e)
+            }, 500
+        
+        return func(current_user, *args, **kwargs)
+    
+    return decorated
+
