41397141b01648080fc2e6ef3aae190b3301c04e
Tonyeseh <esehtony123@gmail.com>
2023-12-23 20:15:05 +0100

    admin damage page js update
    added api call to assign worker and delete damage

--- backend/web_dynamic/admin/static/js/damage.js
+++ backend/web_dynamic/admin/static/js/damage.js
@@ -20,31 +20,88 @@ function getCookie(name) {
     return null;
   }
 
+function deleteHelper(elem) {
+    deleteButton = document.getElementById('deleteButton')
+    deleteButton.setAttribute('data-damage-id', elem.dataset.damageId)
 
-function deleteRecord(element) {
-    
-    fetch(`http://127.0.0.1:5001/api/v1/damages/${element.id}`, {method: "DELETE", headers: {Authorization: `Bearer ${getCookie('admin_access_token')}`}}).then(res => res.json()).then(data => {
+    deleteButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        fetch(`http://127.0.0.1:5001/api/v1/damages/${elem.dataset.damageId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${getCookie('admin_access_token')}`
+            }
+        }).then(res => res.json())
+        .then(data => {
 
-        data.error || element.parentNode.parentNode.remove()
+        data.error || elem.parentNode.parentNode.remove()
+    })
     })
 }
 
-updateRecord = (element) => {
-    workerId = element.parentNode.previousSibling.previousSibling.childNodes[1]
-
-    if (workerId.value && element.id) {
-
-    form = new FormData()
-    form.append('damage_id', element.id)
-    form.append('worker_id', workerId.value)
-
-    fetch('http://127.0.0.1:5001/api/v1/damages/working_on', {
-        body: form,
-        method: 'POST',
+function editRecord(elem) {
+    workerSelect = document.getElementById('workerSelect')
+    fetch(`http://127.0.0.1:5001/api/v1/damages/${elem.dataset.damageId}/workers`, {
         headers: {
             Authorization: `Bearer ${getCookie('admin_access_token')}`
         }
     }).then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        workerSelect.innerHTML = `<option value="">Pick a worker...</option>`
+        data.forEach(elem => {
+            workerSelect.innerHTML += `<option value="${elem.id}">${elem.email}</option>`
+        });
+    })
+
+    
+
+    editButton = document.getElementById('editButton')
+    damageRow = document.getElementById(elem.dataset.damageId)
+    damageState = document.getElementById(`state-${elem.dataset.damageId}`)
+    damageWorker = document.getElementById(`worker-${elem.dataset.damageId}`)
+    editButton.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        if (workerSelect.value) {
+
+        form = new FormData()
+        form.append('worker_id', workerSelect.value)
+
+        fetch(`http://127.0.0.1:5001/api/v1/damages/${elem.dataset.damageId}`, {
+            body: form,
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${getCookie('admin_access_token')}`,
+            }
+        }).then(res => res.json())
+        .then(data => {
+            if (data.data) {
+                damageWorker.innerText = data.data.worker_name
+                damageState.innerHTML = `<span class="badge badge-sm bg-gradient-info">${data.data.damage_state}</span>`
+            }
+        })
+        .catch(e => console.log(e))
+    }
+    })
 }
-}
+
+// updateRecord = (element) => {
+//     workerId = element.parentNode.previousSibling.previousSibling.childNodes[1]
+
+//     if (workerId.value && element.id) {
+
+//     form = new FormData()
+//     form.append('damage_id', element.id)
+//     form.append('worker_id', workerId.value)
+
+//     fetch('http://127.0.0.1:5001/api/v1/damages/working_on', {
+//         body: form,
+//         method: 'POST',
+//         headers: {
+//             Authorization: `Bearer ${getCookie('admin_access_token')}`
+//         }
+//     }).then(res => res.json())
+//     .then(data => console.log(data))
+// }
+// }
